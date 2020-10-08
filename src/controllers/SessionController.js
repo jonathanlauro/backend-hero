a6d5abcdbb58fd02f9a7814923230079dc5001cd
Jonathan Lauro Mendes de Souza <Jonathan@MacBook-Pro-de-Jonathan.local>
2020-10-08 20:56:41 -0300

    finalizado

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -3,15 +3,14 @@ const connectiton = require('../database/connection');
 module.exports = {
   async create(req,res){
     const {id} = req.body;
-    console.log(req.body);
 
-//     // const ong = await connectiton('ongs')
-//     //   .where('id', id)
-//     //   .select('name')
-//     //   .first();
-//     //   if(!ong){ 
-//     //     return res.status(400).json({error: 'No ONG Found with this ID'});
-//     //   }
-//     // return res.json(ong);
+    const ong = await connectiton('ongs')
+      .where('id', id)
+      .select('name')
+      .first();
+      if(!ong){ 
+        return res.status(400).json({error: 'No ONG Found with this ID'});
+      }
+    return res.json(ong);
   }
 }
